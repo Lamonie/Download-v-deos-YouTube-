f60f36e8176b39acd005e1160d6146db1773730a
Lamonie <llamonie@gmail.com>
2026-05-24 10:57:17 -0300

    Update download.js

--- Api/download.js
+++ Api/download.js
@@ -1,10 +1,7 @@
 export default async function handler(req, res) {
-  // Isso apenas valida se o link veio certo
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method !== 'POST') return res.status(405).end();
   const { url } = req.body;
-  if (!url) return res.status(400).json({ error: "URL necessária" });
-
-  // Em vez de esperar o Cobalt baixar, nós apenas redirecionamos
-  // Isso não consome o tempo da Vercel
   res.status(200).json({
     redirect: `https://co.wuk.sh/api/json?url=${encodeURIComponent(url)}`
   });
